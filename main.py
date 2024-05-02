44b2d6df9c6477ed33b7e99b0ad45f0256c6ae59
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-02 18:58:39 +0300

    работа с валидацией данных

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import FastAPI
+from pydantic import Field, BaseModel
 
 app = FastAPI(title='Tradding App')
-
 
 test_bd_user = [{"id": 1, "role": "admin", "name": "Bob1"},
                 {"id": 2, "role": "admin", "name": "Bob2"},
@@ -9,25 +11,28 @@ test_bd_user = [{"id": 1, "role": "admin", "name": "Bob1"},
                 {"id": 4, "role": "admin", "name": "Bob4"}]
 
 test2_bd_user = [{"id": 1, "role": "admin", "name": "Bob1"},
-                {"id": 2, "role": "admin", "name": "Bob2"},
-                {"id": 3, "role": "admin", "name": "Bob3"},
-                {"id": 4, "role": "admin", "name": "Bob4"}]
+                 {"id": 2, "role": "admin", "name": "Bob2"},
+                 {"id": 3, "role": "admin", "name": "Bob3"},
+                 {"id": 4, "role": "admin", "name": "Bob4"}]
 
 test_bd_trades = [{"id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
                   {"id": 2, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
                   {"id": 3, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12}]
+
+
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     return [i for i in test_bd_user if i["id"] == user_id]
 
-@app.post("/users/{user_id}/")
-def get_user(user_id: int, name: str):
-    current_user = list(filter(lambda user: user["id"] == user_id, test2_bd_user))[0]
-    current_user['name'] = name
-    return current_user
+class Trade(BaseModel):
+    id: int
+    currency: str = Field(max_length=10)
+    side: str
+    price: int = Field(gt=0)
+    amount: float
 
-
-# @app.get("/users/")
-# def get_data(limit: int = 1, offset: int = 2):
-#     return test_bd_user[offset:][:limit]
-
+@app.post("/trades/")
+def add_trades(trades: List[Trade]):
+    test_bd_trades.extend(trades)
+    return {"ststus": 200,
+            "data": test_bd_trades}
