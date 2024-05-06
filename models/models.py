03d242bf277cf822f3a2a57d67a09a5c7bd72b59
InnaLik <110997246+InnaLik@users.noreply.github.com>
2024-05-06 17:09:43 +0300

    Подключение к бд

---
+++ models/models.py
@@ -0,0 +1,24 @@
+import datetime
+
+import sqlalchemy
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Column, JSON
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Role(Base):
+    __tablename__ = "roles"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    permission = Column(JSON)
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(Integer, nullable=False)
+    registration_at = Column(TIMESTAMP, default=datetime.datetime.utcnow)
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
